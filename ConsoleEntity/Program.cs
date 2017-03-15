97e8583646a80387f207ad0e373295e2b57c6937
david di Marcantonio <david@code4mac.com>
2017-03-15 15:18:20 +0100

    add using

--- ConsoleEntity/Program.cs
+++ ConsoleEntity/Program.cs
@@ -10,6 +10,59 @@ namespace ConsoleEntity
     {
         static void Main(string[] args)
         {
+
+            using (var db = new CommandesContext() )
+            {
+
+                /* -----------------------------
+                 *         Create 
+                 *         --------------------*/
+
+                produit raquette = new produit { nom = "raquette", description = "une raquette ", prix = 10.20m };
+                db.produits.Add(raquette);
+                db.SaveChanges();
+
+                /* -----------------------------
+                *         Retrieve 
+                *         --------------------*/
+                var listeDeProduits = from prod in db.produits
+                                      orderby prod.nom
+                                      select prod;
+
+                foreach (var item in listeDeProduits)
+                {
+                    Console.WriteLine(item.nom);
+                }
+
+
+                /* -----------------------------
+                 *         Update 
+                 *         --------------------*/
+
+                var findRaquette = from r in db.produits
+                                  where r.nom.Contains("raquette")
+                                  select r;
+                if (findRaquette.Count() >  0)
+                {
+                    var produitAModifier = new produit { id_produit = findRaquette.First().id_produit };
+                    var produitOriginal = db.produits.Find(produitAModifier);
+                    produitOriginal.prix += 10.0m;
+                    db.SaveChanges();
+                }
+
+
+
+                /* -----------------------------
+                *         Delete 
+                *  --------------------*/
+
+                // delete
+                var produitToDelete = new produit { id_produit = 1 };
+                db.produits.Attach(produitToDelete);
+                db.produits.Remove(produitToDelete);
+                db.SaveChanges();
+
+            }
         }
     }
 }
